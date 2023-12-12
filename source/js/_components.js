c9071bdb52bf9d861b62538694841cbdf99b3458
roman <romasaab@gmail.com>
2023-12-12 16:34:22 +0200

    adaptive

--- source/js/_components.js
+++
@@ -1,9 +0,0 @@
-// import './components/sliders';
-import './components/countdown';
-import './components/burger';
-// import './components/getDinamicHeight';
-import './components/accordions';
-
-
-
-
